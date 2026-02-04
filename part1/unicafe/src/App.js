f7e4c725e6988b45a56005145ec1d2385a405159
Murilo Beraldo <murilohenriqueberaldo@gmail.com>
2026-02-03 21:24:08 -0300

    Remove comment
    Removed a comment

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -38,7 +38,6 @@ const Statistics = (props) => {
   )
 }
 const App = () => {
-  // salve os cliques de cada botão em seu próprio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
